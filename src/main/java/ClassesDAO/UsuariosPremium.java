3937d3fe1879911a4f3557dea05321edbfbb79c2
joaoscam <joao.camargo@germinare.org.br>
2024-09-27 11:47:47 -0300

    comitando a pastaDAO

---
+++ src/main/java/ClassesDAO/UsuariosPremium.java
@@ -0,0 +1,9 @@
+package ClassesDAO;
+
+public class UsuariosPremium {
+    private String nome;
+    private String senha;
+    private String email;
+    private String dt_nascimento;
+    private int level;
+}
