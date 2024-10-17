046d7b040116d27dda2f76c841c69ddd0e88ea2e
Vitor Maciel <vitor.maciel@germinare.org.br>
2024-10-17 11:47:09 -0300

    Moedas.javaa

--- src/main/java/org/example/teste/UsuariosPremuim.java
+++ src/main/java/org/example/teste/UsuariosPremuim.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "usuartios_premuim", value = "/usuarios_premuim")
-public class UsuariuosPremuimServelet extends HttpServlet {
+public class UsuariosPremuim extends HttpServlet {
 
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
