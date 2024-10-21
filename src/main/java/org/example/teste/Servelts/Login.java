2359b147d937bae829b9736904040be0a6876f04
Arthur Formigoni <arthur.cruz@germinare.org.br>
2024-10-21 09:12:26 -0300

    mudando o login

--- src/main/java/org/example/teste/Servelts/Login.java
+++ src/main/java/org/example/teste/Servelts/Login.java
@@ -5,8 +5,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.teste.Model.Adm_;
+import org.example.teste.Model.Powerup;
 
 import java.io.*;
+import java.sql.*;
 
 @WebServlet(name="adm",value = "/login")
 public class Login extends HttpServlet {
@@ -14,16 +17,27 @@ public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String user = req.getParameter("txt");
         String senha = req.getParameter("pswd");
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://pg-23037034-germinare-1db6.f.aivencloud.com:27088/dbDelfis?ssl=require&user=avnadmin&password=AVNS_IUFw8-OfVH7bf8zuL_l");
+            String sql = "select * from adm";  // Substitua com a sua tabela de usuários
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
 
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
+            while (rs.next()) {
+                Adm_ adm = new Adm_();
+                adm.setSenha(rs.getString("senha"));
+                adm.setLogin(rs.getString("login"));
+                if (user.equals(adm.getLogin()) && senha.equals(adm.getSenha())) {
+                    System.out.println("a");
+                    req.getRequestDispatcher("HTML/home_crud.html").forward(req, resp);
+                }
+            }
 
-        if (user.equals("ADM") && senha.equals("123")){
-            req.getRequestDispatcher("HTML/home_crud.html").forward(req, resp);
-        }else {
-            out.println("<html><body>");
-            out.println("<h1> Senha incorreta </h1>");
-            out.println("</body></html>");
+        }catch (SQLException  | ClassNotFoundException a){
+            a.printStackTrace();
         }
     }
 }
