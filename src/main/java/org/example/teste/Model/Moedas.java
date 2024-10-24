100e9ccde59e9f6ec412883fd5c1460e5698058f
Vitor Maciel <vitor.maciel@germinare.org.br>
2024-10-24 12:34:40 -0300

    Moedas comentada

--- src/main/java/org/example/teste/Model/Moedas.java
+++ src/main/java/org/example/teste/Model/Moedas.java
@@ -1,11 +1,19 @@
-
+//Importações - Início
 package org.example.teste.Model;
+//Importações - Fim
+
+//Classe - Início
 
 public class Moedas {
+    //Atributos - Início
     private int quantidade;
     private int id_moedas;
     private int fk_usuario;
     private int fk_pagamento;
+    //Atributos - Fim
+
+    //Métodos - Início
+    //Construtor - Início
 
     public Moedas(){}
     public Moedas(int quantidade, int id_moedas, int fk_usuario, int fk_pagamento){
@@ -14,6 +22,10 @@ public class Moedas {
         this.fk_usuario = fk_usuario;
         this.fk_pagamento = fk_pagamento;
     }
+    //Construtor - Fim
+
+
+    //Getters e Setters - Início
 
     public int getQuantidade() {
         return quantidade;
@@ -46,6 +58,9 @@ public class Moedas {
     public void setFk_pagamento(int fk_pagamento) {
         this.fk_pagamento = fk_pagamento;
     }
+    //Getters e Setters - Fim
+
+    //toString - Início
     @Override
     public String toString() {
         return "Coins{" +
@@ -56,6 +71,5 @@ public class Moedas {
                 '}';
     }
 
-
-
-}
+    // toString - Fim
+}//Métodos e Classe - Fim
