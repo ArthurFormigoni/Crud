1131e677071b9ddab6403128323a78353da88594
V8bits <vitor.riter@germinare.org.br>
2024-10-25 07:43:58 -0300

    MoedasPagamentoMoedas.java comentado.

--- src/main/java/org/example/teste/Servelts/Home.java
+++ src/main/java/org/example/teste/Servelts/Home.java
@@ -1,3 +1,5 @@
+//Importações  - Início
+
 package org.example.teste.Servelts;
 
 import jakarta.servlet.ServletException;
@@ -7,10 +9,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.*;
+//Importações - Fim
 
 @WebServlet(name="home",value = "/home")
 public class Home extends HttpServlet {
     @Override
+    // Métodos - Início
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // Define o tipo de conteúdo da resposta como HTML
         resp.setContentType("text/html");
@@ -18,4 +22,4 @@ public class Home extends HttpServlet {
         // Encaminha a requisição e a resposta para a página "home_crud.html" localizada na pasta "HTML"
         req.getRequestDispatcher("/HTML/home_crud.html").forward(req, resp);
     }
-}
+}//Métodos e Classe - Fim
